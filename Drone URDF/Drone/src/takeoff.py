f39d886bd7dc79d920ed664b7776579a54c254cd
Toshan <toshan1603@gmail.com>
2021-09-29 11:58:36 +0530

    Added basic velocity joint interfaces and plugin

---
+++ Drone URDF/Drone/src/takeoff.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python3
+import rospy
+from std_msgs.msg import Empty
+from geometry_msgs.msg import Twist
+
+
+def fly():
+    rospy.init_node('my_node_takeoff')
+
+    empty = Empty()
+    var_twist = Twist()
+    # pub_position = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+    # var_twist.linear.x = 3
+    # var_twist.linear.y = 3
+    # var_twist.linear.z = -2
+    takeoff = rospy.Publisher('/drone/takeoff', Empty, queue_size=1)
+    
+    while not rospy.is_shutdown():
+        takeoff.publish(empty)
+        #pass
+        #pub_position.publish(var_twist)
+
+if __name__ == '__main__':
+    try:
+        fly()
+    except rospy.ROSInterruptException:
+        pass
+
+
+#pub_position.publish(var_twist)
+
+
