a934dec36cd9f7dcc04be66a8ed059311f38af2c
Jash-Shah <jash28582@gmail.com>
2021-10-11 12:10:53 +0530

    Fixed pid for rpy. Still need to work on pitch pid

--- Drone URDF/vitarana_drone/scripts/control.py
+++ Drone URDF/vitarana_drone/scripts/control.py
@@ -2,7 +2,6 @@
 import rospy
 import time
 from pid import PID_alt
-#import read_sensor_data
 import message_filters
 from vitarana_drone.msg import prop_speed
 from rospy.topics import Publisher
@@ -13,68 +12,89 @@ from geometry_msgs.msg import Vector3
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
 from std_msgs.msg import Float64, Float64MultiArray
 
+
+# Initilization of all Parameters
 altitude = 0.30999
-kp = 25
-ki = 0.05
-kd = 20
-kp_roll = 0.2
-ki_roll = 0.001
-kd_roll = 0.5
-kp_pitch = 0.15
-ki_pitch = 0.001
-kd_pitch = 0.4
-kp_yaw = 0.1
-ki_yaw = 0.001
-kd_yaw = 0.2
-flag = 0
+thrust = 0
 vel_x = 0 
 vel_y = 0 
 vel_z = 0
 roll = 0
 pitch = 0
 yaw = 0
-message_pub = rospy.Publisher("/edrone/pwm", prop_speed, queue_size=1000)
-req_alt = float(input("Enter height drone should hover at : "))
-thrust = 0
 
+# Giving default PID values incase no input from user
+kp = 20
+ki = 0.001
+kd = 35
+kp_roll = 0.2
+ki_roll = 0.00001
+kd_roll = 0.5
+kp_pitch = 0.1
+ki_pitch = 0.001
+kd_pitch = 10
+kp_yaw = 50
+ki_yaw = 0.01
+kd_yaw = 5
+
+# Flag for checking for the first time the script is run
+flag = 0
+
+# Message to publish final motor speeds to propellers
+message_pub = rospy.Publisher("/edrone/pwm", prop_speed, queue_size=1000)
+
+# Ask the user for the required height the drone should hover at
+req_alt = float(input("Enter height drone should hover at : "))
+
+
+# Gets altitude PID published to node
 def setPID_alt(msg):
     global kp,ki,kd
     kp = msg.data[0]
     ki =  msg.data[1]
     kd = msg.data[2]
 
+
+# Gets roll PID published to node
 def setPID_roll(msg):
     global kp_roll,ki_roll,kd_roll
     kp_roll = msg.data[0]
     ki_roll =  msg.data[1]
     kd_roll = msg.data[2]
 
+
+# Gets pitch PID published to node
 def setPID_pitch(msg):
     global kp_pitch,ki_pitch,kd_pitch
     kp_pitch = msg.data[0]
     ki_pitch =  msg.data[1]
     kd_pitch = msg.data[2]
 
+
+# Gets yaw PID published to node
 def setPID_yaw(msg):
     global kp_yaw,ki_yaw,kd_yaw
     kp_yaw = msg.data[0]
     ki_yaw =  msg.data[1]
     kd_yaw = msg.data[2]
 
+
+# Gets current altitude  of drone from gps sensor
 def calAltitude(msg):
     global altitude
     altitude  = msg.altitude
     rospy.loginfo("\nAltitude = " + str(altitude))
-    #return altitude
 
+
+# Gets current velocity  of drone from gps_vel sensor
 def calVelocity(msg):
     global vel_x, vel_y, vel_z
     vel_x = msg.vector.x
     vel_y = msg.vector.y
     vel_z = msg.vector.z
-    #rospy.loginfo("\nVx = {0}\nVy = {1}\nVz = {2}\n".format(vel_x,vel_y,vel_z))
-    #return (vel_x, vel_y, vel_z)
 
+
+# Gets current roll. pitch, yaw of drone from IMU sensor
 def calImu(msg):
     orinetation_list = [msg.orientation.x,msg.orientation.y,msg.orientation.z,msg.orientation.w]
     global roll, pitch, yaw 
@@ -85,158 +105,67 @@ def calImu(msg):
     angVel_x = msg.angular_velocity.x
     angVel_y = msg.angular_velocity.y
     angVel_z = msg.angular_velocity.z
-    #rospy.loginfo("\nRoll = {0}\nPitch = {1}\nYaw = {2}\n".format(roll,pitch,yaw))    
-    #rospy.loginfo("\nWx = {0}\nWy = {1}\nWz = {2}\n".format(angVel_x,angVel_y,angVel_z))
-    
-    #return (roll, pitch, yaw)
-
-
-
-def PID_alts(gps, vel, imu):
-    
-    global altitude #!!
-    global req_alt
-    global flag
-    global thrust
-    global speed
-    global rate
-    global prev_time,prev_alt_err,i_term,d_term,p_term
-    global roll, pitch, yaw
-    global flag
-    altitude  = gps.altitude
-    print("\nAltitude = " + str(altitude))
-    current_alt_err = req_alt - altitude
-    print("Required alt = ",req_alt)
-    # rospy.init_node("pid_alt_node",anonymous=False)
-    rospy.Subscriber("alt_pid", Float64MultiArray, setPID) #!!
-    #calVelocity(vel)
-    #calImu(imu)
-
-    
-    # print("roll - control",roll)
-    # print("pitch - control",pitch)
-    # print("yaw - control",yaw)
-    
-    print("kp = ",kp)
-    print("ki = ",ki)
-    print("kd = ",kd)
-    hover_speed = 508.75
-    sample_time = 0
-    current_time = time.time()
-    # print("Current time = ",current_time)
-    if flag == 0:
-        prev_time = 0
-        prev_alt_err = 0
-        i_term = 0
-        d_term = 0
-        
-
-    dTime = current_time - prev_time
-    dErr_alt = current_alt_err -prev_alt_err
-
-    if (dTime >= sample_time):
-        if flag==0:
-            p_term = 0
-            i_term = 0
-            d_term = 0
-            flag+=1
-        else:
-            p_term = current_alt_err
-            i_term += current_alt_err * dTime
-            d_term =  dErr_alt/dTime
-
-    prev_time = current_time
-    prev_alt_err = current_alt_err
-
-    output_alt = kp*p_term + ki*i_term + kd*d_term
-
-    print("Altitude Correction = ",output_alt)
-    thrust = hover_speed + output_alt*10
-
-    #we need to limit this thrust
-    if(thrust > 1000): 
-        thrust = 1000
-    elif(thrust < 0):
-        thrust = 0
-    
-    print("Thrust = ",thrust)
-
-    speed = prop_speed()
-    speed.prop1 = thrust
-    speed.prop2 = thrust
-    speed.prop3 = thrust
-    speed.prop4 = thrust
-    rospy.loginfo(speed) #!!!
-    message_pub.publish(speed)
-
-    # while not rospy.is_shutdown():
-    #     message_pub.publish(speed)
 
 
 def alt_control(gps, vel, imu):
-    global altitude #!!
-    global req_alt
-    global flag
-    global kp,ki,kd
-    # global thrust
-    # global speed
-    global roll, pitch, yaw
+    # Set all variables to global so as to keep them updated values
+    global altitude,req_alt,flag, kp,ki,kd,roll, pitch, yaw
 
+    # Gets drones current velocity
     calVelocity(vel)
+    # Gets drones current rpy
     calImu(imu)
+    # Gets drones current altitude
     calAltitude(gps)
-    rospy.Subscriber("alt_pid", Float64MultiArray, setPID_alt) #!!
-    rospy.Subscriber("roll_pid", Float64MultiArray, setPID_roll) #!!
-    rospy.Subscriber("pitch_pid", Float64MultiArray, setPID_pitch) #!!
-    rospy.Subscriber("yaw_pid", Float64MultiArray, setPID_yaw) #!!
+
+    # Subsribe to all required topics to get PID for all controllers
+    rospy.Subscriber("alt_pid", Float64MultiArray, setPID_alt) 
+    rospy.Subscriber("roll_pid", Float64MultiArray, setPID_roll) 
+    rospy.Subscriber("pitch_pid", Float64MultiArray, setPID_pitch) 
+    rospy.Subscriber("yaw_pid", Float64MultiArray, setPID_yaw) 
+
+    # Combine the PID values into tuples so as to send easily to PID function
     k_alt = (kp,ki,kd)
     k_roll = (kp_roll,ki_roll,kd_roll)
     k_pitch = (kp_pitch,ki_pitch,kd_pitch)
     k_yaw = (kp_yaw,ki_yaw,kd_yaw)
+
+    # Logging for debugging purposes
     print("\nAltitude = " + str(altitude))
-    current_alt_err = req_alt - altitude
     print("Required alt = ",req_alt)
     print("Roll =", roll)
     print("Pitch =", pitch)
     print("Yaw =", yaw)
-    # rospy.init_node("pid_alt_node",anonymous=False)
-    #rospy.Subscriber("alt_pid", Float64MultiArray, setPID) #!!
-    
-    #get information of the velocity and r p y of the drone
     
     #the goal is to get a function that stabilises the r p y of the drone while maintaining altitude
+    #speed returned is the final motor speed after going through the motor mixing algorithm for all controllers
     speed = PID_alt(roll, pitch, yaw, req_alt, altitude, k_alt, k_roll, k_pitch, k_yaw, flag)
-    flag += 1
+    flag += 1 
+
+    # Publish the final motor speeds to the propellers
     message_pub.publish(speed)
 
 
-
-
-
 def control():
+    # define global values for required parameters to avoid resetting to 0
     global altitude, thrust, speed
-    rospy.init_node("altitude", anonymous = False)
+
+    # initialize node
+    rospy.init_node("altitude", anonymous = False)  
+
+    # Creating subscribers to get all relevant sensor data
     gps_sub = message_filters.Subscriber("/edrone/gps", NavSatFix)
     vel_sub = message_filters.Subscriber("/edrone/gps_velocity", Vector3Stamped)
     imu_sub = message_filters.Subscriber("/edrone/imu/data", Imu)
     ts = message_filters.TimeSynchronizer([gps_sub, vel_sub, imu_sub], 2)
+
     #one of these publishers is slower than the others
     #which is why the messages are loading relatively slowly
     ts.registerCallback(alt_control)
-
     rospy.spin()
-    # rospy.init_node("motor_speed_pub",anonymous=False)
-    # speed = prop_speed()
-    # speed.prop1 = thrust
-    # speed.prop2 = thrust
-    # speed.prop3 = thrust
-    # speed.prop4 = thrust
-    # rospy.loginfo(speed) #!!!
-    # while not rospy.is_shutdown():
-    #     message_pub.publish(speed)
        
 
-
+# Main function
 if __name__=='__main__':
     try:
         control()
@@ -300,7 +229,85 @@ if __name__=='__main__':
     
 
 
+# def PID_alts(gps, vel, imu):
+    
+#     global altitude #!!
+#     global req_alt
+#     global flag
+#     global thrust
+#     global speed
+#     global rate
+#     global prev_time,prev_alt_err,i_term,d_term,p_term
+#     global roll, pitch, yaw
+#     global flag
+#     altitude  = gps.altitude
+#     print("\nAltitude = " + str(altitude))
+#     current_alt_err = req_alt - altitude
+#     print("Required alt = ",req_alt)
+#     # rospy.init_node("pid_alt_node",anonymous=False)
+#     rospy.Subscriber("alt_pid", Float64MultiArray, setPID) #!!
+#     #calVelocity(vel)
+#     #calImu(imu)
 
+    
+#     # print("roll - control",roll)
+#     # print("pitch - control",pitch)
+#     # print("yaw - control",yaw)
+    
+#     print("kp = ",kp)
+#     print("ki = ",ki)
+#     print("kd = ",kd)
+#     hover_speed = 508.75
+#     sample_time = 0
+#     current_time = time.time()
+#     # print("Current time = ",current_time)
+#     if flag == 0:
+#         prev_time = 0
+#         prev_alt_err = 0
+#         i_term = 0
+#         d_term = 0
+        
+
+#     dTime = current_time - prev_time
+#     dErr_alt = current_alt_err -prev_alt_err
+
+#     if (dTime >= sample_time):
+#         if flag==0:
+#             p_term = 0
+#             i_term = 0
+#             d_term = 0
+#             flag+=1
+#         else:
+#             p_term = current_alt_err
+#             i_term += current_alt_err * dTime
+#             d_term =  dErr_alt/dTime
+
+#     prev_time = current_time
+#     prev_alt_err = current_alt_err
+
+#     output_alt = kp*p_term + ki*i_term + kd*d_term
+
+#     print("Altitude Correction = ",output_alt)
+#     thrust = hover_speed + output_alt*10
+
+#     #we need to limit this thrust
+#     if(thrust > 1000): 
+#         thrust = 1000
+#     elif(thrust < 0):
+#         thrust = 0
+    
+#     print("Thrust = ",thrust)
+
+#     speed = prop_speed()
+#     speed.prop1 = thrust
+#     speed.prop2 = thrust
+#     speed.prop3 = thrust
+#     speed.prop4 = thrust
+#     rospy.loginfo(speed) #!!!
+#     message_pub.publish(speed)
+
+#     # while not rospy.is_shutdown():
+#     #     message_pub.publish(speed)
 
 
 
--- Drone URDF/vitarana_drone/scripts/pid.py
+++ Drone URDF/vitarana_drone/scripts/pid.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python3
 import rospy
 import time
-#import read_sensor_data
 from vitarana_drone.msg import prop_speed
 from rospy.topics import Publisher
 from sensor_msgs.msg import NavSatFix
@@ -12,13 +11,14 @@ from tf.transformations import euler_from_quaternion, quaternion_from_euler
 from std_msgs.msg import Float64, Float64MultiArray
 
 
-def PID_alt(roll, pitch, yaw, req_alt, altitude, k_alt, k_roll, k_pitch, k_yaw, flag):
+def PID_alt(roll, pitch, yaw, req_alt, altitude, k_alt, k_roll, k_pitch, k_yaw, flag): 
     #global variables are declared to avoid their values resetting to 0
-    #global altitude #!!
-    global prev_alt_err,i_term,d_term,p_term
+    global prev_alt_err,iMem_alt,dMem_alt,pMem_alt
     global prevTime,kp_roll, ki_roll, kd_roll, kp_pitch, ki_pitch, kd_pitch, kp_yaw, ki_yaw, kd_yaw, prevErr_roll, prevErr_pitch, prevErr_yaw, pMem_roll, pMem_yaw, pMem_pitch, iMem_roll, iMem_pitch, iMem_yaw, dMem_roll, dMem_pitch, dMem_yaw, setpoint, sample_time
     global kp_thrust, ki_thrust, kd_thrust
+
     #-----------------------
+
     #Assigning PID values here. From symmetry, control for roll and pitch is the same.
     kp_thrust = k_alt[0]
     ki_thrust = k_alt[1]
@@ -32,33 +32,30 @@ def PID_alt(roll, pitch, yaw, req_alt, altitude, k_alt, k_roll, k_pitch, k_yaw, 
     kp_yaw = k_yaw[0]
     ki_yaw = k_yaw[1]
     kd_yaw = k_yaw[2]
-    #print("\nAltitude = " + str(altitude))
-    #print("Required alt = ",req_alt)
-    # rospy.init_node("pid_alt_node",anonymous=False)
-    #rospy.Subscriber("alt_pid", Float64MultiArray, setPID) #!!
-    #calVelocity(vel)
-    #calImu(imu)
-
-    
-    
-    # print("roll - control",roll)
-    # print("pitch - control",pitch)
-    # print("yaw - control",yaw)
-    
-    # print("kp = ",kp)
-    # print("ki = ",ki)
-    # print("kd = ",kd)
-
     setpoint = 0 #this should change according to the desired r,p,y
-    # I assume, not 100% sure
+
+    #100% sure
     err_pitch = pitch - setpoint
     err_roll = roll - setpoint
-    err_yaw = yaw - setpoint
+    err_yaw = setpoint - yaw
     current_alt_err = req_alt - altitude
-    hover_speed = 508.75
-    sample_time = 0
+
+    # Publishing error values to be plotted in rqt
+    alt_err_pub = rospy.Publisher("/alt_err", Float64, queue_size=10)
+    alt_err_pub.publish(current_alt_err)
+    roll_err_pub = rospy.Publisher("/roll_err", Float64, queue_size=10)
+    roll_err_pub.publish(err_roll)
+    pitch_err_pub = rospy.Publisher("/pitch_err", Float64, queue_size=10)
+    pitch_err_pub.publish(err_pitch)
+    yaw_err_pub = rospy.Publisher("/yaw_err", Float64, queue_size=10)
+    yaw_err_pub.publish(err_yaw)
+    sample_time = 0.01
     current_time = time.time()
-    # print("Current time = ",current_time)
+
+    #Speed found from testing at which drone hovers at a fixed height
+    hover_speed = 508.75 
+  
+    # Flag for checking for the first time the function is called so that values can initilized
     if flag == 0:
         prevTime = 0
         prevErr_roll = 0
@@ -74,97 +71,98 @@ def PID_alt(roll, pitch, yaw, req_alt, altitude, k_alt, k_roll, k_pitch, k_yaw, 
         dMem_pitch = 0
         dMem_yaw = 0    
         prev_alt_err = 0
-        i_term = 0
-        d_term = 0
+        iMem_alt = 0
+        dMem_alt = 0
 
-    #Define the difference in error or dErr
-    
-    
-
+    #Define all differential terms
     dTime = current_time - prevTime 
-    dErr_alt = current_alt_err - prev_alt_err #difference in error
+    print("dTime = ",dTime)
+    dErr_alt = current_alt_err - prev_alt_err 
     dErr_pitch = err_pitch - prevErr_pitch
     dErr_roll = err_roll - prevErr_roll
     dErr_yaw = err_yaw - prevErr_yaw
     
     #--------------------------------
     if (dTime >= sample_time):
-        p_term = current_alt_err#this is for thrust
-        # if flag == 0:
-        #     flag += 1
-        # else:
-        #proportional(e(t))
+        # Proportional terms
+        pMem_alt = current_alt_err#this is for thrust
         pMem_roll = kp_roll * err_roll
         pMem_pitch = kp_pitch * err_pitch
         pMem_yaw = kp_yaw * err_yaw
-        #integral(e(t))
-        i_term += current_alt_err * dTime #this is for thrust
-        iMem_roll += err_pitch * dTime
-        iMem_pitch += err_roll * dTime
+
+        #Integral Terms(e(t))
+        iMem_alt += current_alt_err * dTime #this is for thrust
+        iMem_roll += err_roll* dTime
+        iMem_pitch += err_pitch * dTime
         iMem_yaw += err_yaw * dTime
         #limit integrand values
-        if(i_term > 800): i_term = 800
-        if(i_term < -800): i_term = -800
+        if(iMem_alt > 800): iMem_alt = 800
+        if(iMem_alt < -800): iMem_alt = -800
         if(iMem_roll > 400): iMem_roll = 400
         if(iMem_roll < -400): iMem_roll = -400
         if(iMem_pitch > 400): iMem_pitch = 400
         if(iMem_pitch < -400): iMem_pitch = -400
         if(iMem_yaw > 400): iMem_yaw = 400
         if(iMem_yaw < -400): iMem_yaw = 400
-        #derivative(e(t))
+
+        #Derivative Terms(e(t))
         dMem_roll = dErr_roll / dTime
         dMem_pitch = dErr_pitch / dTime
         dMem_yaw = dErr_yaw / dTime
-        #d_term =  dErr_alt / dTime #if dTime is very small, this term will be very large
-        d_term = dErr_alt / dTime
+        dMem_alt = dErr_alt / dTime
+        
+        prevTime = current_time
 
-    prevTime = current_time
-    print("Prev Time = ",prevTime)
+    # Updating all prev terms for next iteration
     prevErr_roll = err_roll
     prevErr_pitch = err_pitch
     prevErr_yaw = err_yaw
     prev_alt_err = current_alt_err
 
-    output_alt = kp_thrust*p_term + ki_thrust*i_term + kd_thrust*d_term
+    # Final output correction terms after combining PID
+    output_alt = kp_thrust*pMem_alt + ki_thrust*iMem_alt + kd_thrust*dMem_alt
     output_roll = pMem_roll + ki_roll * iMem_roll + kd_roll * dMem_roll
     output_pitch = pMem_pitch + ki_pitch * iMem_pitch + kd_pitch * dMem_pitch
     output_yaw = pMem_yaw + ki_yaw * iMem_yaw + kd_yaw * dMem_yaw 
 
-    print("Altitude Correction = ",output_alt)
+    # For Debugging Purposes
+    # print("D Time = ",dTime)
     # print("Flag = ",flag)
-    print("D Time = ",dTime)
-    # print("P Term = ",p_term)
-    # print("I Term = ",i_term)
-    # print("D Term = ",d_term)
-    print("P Term = ",pMem_roll)
-    print("I Term = ",iMem_roll)
-    print("D Term = ",dMem_roll)
-    print("Roll Correction = ",output_roll)
+    # print("P Term Alt= ",pMem_alt)
+    # print("I Term Alt= ",iMem_alt)
+    # print("D Term Alt= ",dMem_alt)
+    print("Altitude Error = ",current_alt_err)
+    # print("Altitude Correction = ",output_alt)
+    # print("P Term Roll= ",pMem_roll)
+    # print("I Term Roll= ",iMem_roll)
+    # print("D Term Roll= ",dMem_roll)
+    print("Roll Error = ",err_roll)
+    # print("Roll Correction = ",output_roll)
+    print("P Term Pitch= ",pMem_pitch)
+    print("I Term Pitch= ",iMem_pitch)
+    print("D Term Pitch= ",dMem_pitch)
+    print("Pitch Error = ",err_pitch)
+    # print("Pitch Correction = ",output_pitch)
+    # print("P Term Yaw= ",pMem_yaw)
+    # print("I Term Yaw= ",iMem_yaw)
+    # print("D Term Yaw= ",dMem_yaw)
+    print("Yaw Error = ",err_yaw)
+    # print("Yaw Correction = ",output_yaw)
 
+    # Final thrust
     thrust = hover_speed + output_alt*2.5
-
-
-    #we need to limit this thrust
+    #Limiting thrust
     if(thrust > 1000): 
         thrust = 1000
     elif(thrust < 0):
-        thrust = 0
-    
+        thrust = 0    
     print("Thrust = ",thrust)
 
     speed = prop_speed()
-    # speed.prop1 = thrust
-    # speed.prop2 = thrust
-    # speed.prop3 = thrust
-    # speed.prop4 = thrust
-
-    #values coming out are strange
-    #Need to fine tune a lot
-
     #uncomment for only altitutde PID testing
-    #output_roll=0 commented to test roll stabilisation
+    # output_roll=0 
     output_pitch=0
-    output_yaw=0
+    # output_yaw=0
 
 
     speed.prop1 = (thrust - output_yaw + output_pitch - output_roll) 
@@ -185,6 +183,6 @@ def PID_alt(roll, pitch, yaw, req_alt, altitude, k_alt, k_roll, k_pitch, k_yaw, 
     if(speed.prop2 < 0): speed.prop2 = 0
     if(speed.prop3 < 0): speed.prop3 = 0
     if(speed.prop4 < 0): speed.prop4 = 0 
-    rospy.loginfo(speed) #!!
+    rospy.loginfo(speed) 
+
     return(speed)
-    #message_pub.publish(speed)
